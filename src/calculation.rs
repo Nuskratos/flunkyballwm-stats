0234fc0195efe940376b817e5a4647560684b176
schmidt <schmi@SCHLACHTSCHIFF>
2024-05-08 15:02:35 +0200

    Finished side stats

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,40 +1,5 @@
 use std::collections::HashMap;
-use crate::data::{Game, Team, TeamMember};
-
-struct Accuracy {
-    name: &'static str,
-    throws: u32,
-    hits: u32,
-}
-
-impl Accuracy {
-    fn percentage(&self) -> f32 {
-        self.hits as f32 / self.throws as f32 * 100.0
-    }
-}
-
-fn team_id_from_player(playerid: u32, teams: &Vec<Team>) -> u32 {
-    for team in teams {
-        if (team.member_1.id == playerid || team.member_2.id == playerid) {
-            return team.id;
-        }
-    }
-    0
-}
-
-fn name_from_id(id: u32, teams: &Vec<Team>, players: &Vec<TeamMember>) -> &'static str {
-    for team in teams {
-        if team.id == id {
-            return team.name;
-        }
-    }
-    for player in players {
-        if player.id == id {
-            return player.name;
-        }
-    }
-    "Name not found"
-}
+use crate::data::{Game, Team, TeamMember, AdditionalType};
 
 pub fn print_throwing_accuracy(games: &Vec<Game>, teams: &Vec<Team>, players: &Vec<TeamMember>) {
     let mut throws = 0;
@@ -80,9 +45,108 @@ pub fn print_throwing_accuracy(games: &Vec<Game>, teams: &Vec<Team>, players: &V
     }
     print_line_break();
 }
-fn print_accuracy(accuracy: &Accuracy){
+
+pub fn print_side_information(games: &Vec<Game>) {
+    //TODO maybe extract all those values into a hashmap for easier readability?
+    let mut left_wins = 0;
+    let mut right_wins = 0;
+    let mut left_points = 0;
+    let mut right_points = 0;
+    let mut left_hits = 0;
+    let mut right_hits = 0;
+    let mut left_throws = 0;
+    let mut right_throws = 0;
+    let mut throws = 0;
+    let mut schluck_right = 0;
+    let mut schluck_left = 0;
+    let mut beer_right = 0;
+    let mut beer_left = 0;
+    for game in games {
+        if game.result.points_left > game.result.points_right {
+            left_wins += 1;
+        } else {
+            right_wins += 1;
+        }
+        left_points += game.result.points_left;
+        right_points += game.result.points_right;
+        for round in &game.rounds {
+            throws += 1;
+            if round.thrower.id == game.left_1.id || round.thrower.id == game.left_2.id {
+                left_throws += 1;
+                if (round.hit) {
+                    left_hits += 1;
+                }
+            } else {
+                right_throws += 1;
+                if round.hit {
+                    right_hits += 1;
+                }
+            }
+            for additional in &round.additionals {
+                match &additional.kind {
+                    AdditionalType::STRAFSCHLUCK => {
+                        if additional.source.id == game.left_1.id || additional.source.id == game.left_2.id {
+                            schluck_left += 1;
+                        }else{
+                            schluck_right += 1;
+                        }
+                    }
+                    AdditionalType::STRAFBIER => {
+                        if additional.source.id == game.left_1.id || additional.source.id == game.left_2.id {
+                            beer_left += 1;
+                        }else{
+                            beer_right += 1;
+                        }}
+                    _ => {}
+                }
+            }
+        }
+    }
+    println!("Seite  | Siege | Punkte | Trefferwahrscheinlichkeit | StrafS | StrafB");
+    println!("Links  | {:<5} | {:<5}  | {:.2} = {:>5} von {:<5}   | {:<5}  | {:<5}", left_wins, left_points, left_hits as f32 / left_throws as f32 * 100.0, left_hits, left_throws, schluck_left, beer_left);
+    println!("Rechts | {:<5} | {:<5}  | {:.2} = {:>5} von {:<5}   | {:<5}  | {:<5}", right_wins, right_points, right_hits as f32 / right_throws as f32 * 100.0, right_hits, right_throws, schluck_right, beer_right);
+}
+
+
+struct Accuracy {
+    name: &'static str,
+    throws: u32,
+    hits: u32,
+}
+
+impl Accuracy {
+    fn percentage(&self) -> f32 {
+        self.hits as f32 / self.throws as f32 * 100.0
+    }
+}
+
+fn team_id_from_player(playerid: u32, teams: &Vec<Team>) -> u32 {
+    for team in teams {
+        if (team.member_1.id == playerid || team.member_2.id == playerid) {
+            return team.id;
+        }
+    }
+    0
+}
+
+fn name_from_id(id: u32, teams: &Vec<Team>, players: &Vec<TeamMember>) -> &'static str {
+    for team in teams {
+        if team.id == id {
+            return team.name;
+        }
+    }
+    for player in players {
+        if player.id == id {
+            return player.name;
+        }
+    }
+    "Name not found"
+}
+
+fn print_accuracy(accuracy: &Accuracy) {
     println!("{:<30} threw: {:>3} and hit: {:>3} which is {:<4.2}%", accuracy.name, accuracy.throws, accuracy.hits, accuracy.percentage());
 }
-fn print_line_break(){
+
+fn print_line_break() {
     println!("{:-<70}", "-")
 }
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,15 @@ mod calculation;
 use std::fmt::Debug;
 use data::*;
 use wm24::*;
-use crate::calculation::print_throwing_accuracy;
+use crate::calculation::{print_side_information, print_throwing_accuracy};
 use crate::team_player_data::{BEEF, CHRIS, DA_HAM_SIE, DOS_BROS, FLO, GEWERTET, HANNES, JEROME, JONAS, LAURA, LUISE, MALTE, SASCHA, SEBI, STRAMMSEIN, TOBIAS, WEDELMEDEL, WHITE_CLAW};
 
 fn main() {
     let all_players: Vec<TeamMember> = vec![JEROME, BEEF, SEBI, FLO, SASCHA, JONAS, LUISE, TOBIAS, MALTE, CHRIS, HANNES, LAURA];
     let all_teams: Vec<Team> = vec![DA_HAM_SIE, DOS_BROS, STRAMMSEIN, WHITE_CLAW, WEDELMEDEL, GEWERTET];
     let games_24 = create_all_games_wm_2024();
-    print_throwing_accuracy(&games_24, &all_teams, &all_players);
+    //print_throwing_accuracy(&games_24, &all_teams, &all_players);
+    print_side_information(&games_24);
 }
 
 /*
